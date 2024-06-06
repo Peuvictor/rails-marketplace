44cbbe2fc410d9b9bfa9888bf5de01e54195f8c0
Matheus Estevam <matheus.estevam.br@gmail.com>
2024-06-06 13:49:34 -0300

    add photo no formulário

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_145043) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_153157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_145043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.jsonb "photos"
     t.index ["user_id"], name: "index_shirts_on_user_id"
   end
 
